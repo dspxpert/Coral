a3d16e00f08a723cef83cf5a7c8e5608de0a26aa
dspxpert <32154318+dspxpert@users.noreply.github.com>
2021-06-23 17:24:49 +0900

    Update pydubwavplay.py

--- pyaudio/pydubwavplay.py
+++ pyaudio/pydubwavplay.py
@@ -1,5 +1,6 @@
 from pydub import AudioSegment
 from pydub.playback import play
 
-sound = AudioSegment.from_file('winxp.wav', 'wav')
+sound = AudioSegment.from_wav('winxp.wav')
+#sound = AudioSegment.from_file('winxp.wav', 'wav')
 play(sound)
