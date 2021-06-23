6994a827acff419fe7ecb037bb529b9c801aca71
dspxpert <32154318+dspxpert@users.noreply.github.com>
2021-06-23 17:11:16 +0900

    Add files via upload

---
+++ pyaudio/ttsPlay.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+# pip install gTTS pydub
+# need ffmpeg and ffprobe, install ffmpeg from https://www.gyan.dev/ffmpeg/builds/
+
+from gtts import gTTS
+from io import BytesIO
+from pydub import AudioSegment
+from pydub.playback import play
+
+def ttsPlay(message, lang='en', Display=True):
+    if Display==True:
+        print(message)
+    tts = gTTS(message, lang=lang)
+    fp = BytesIO()
+    tts.write_to_fp(fp)
+    fp.seek(0)
+    song = AudioSegment.from_file(fp, format="mp3")
+    play(song)
+
+if __name__ == "__main__":
+    ttsPlay('Thank you.')
+    ttsPlay('감사합니다.', 'ko')
