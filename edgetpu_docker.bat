09304fb0351980ff6c45414459de230c3fddefe0
dspxpert <32154318+dspxpert@users.noreply.github.com>
2021-06-24 23:02:50 +0900

    Update edgetpu_docker.bat

--- edgetpu_docker.bat
+++ edgetpu_docker.bat
@@ -1,4 +1,4 @@
 @echo off
 echo To build container, docker build --tag edgetpu_compiler https://github.com/tomassams/docker-edgetpu-compiler.git
 echo To run container, docker run -it --rm -v /your/modelpath/:/home/edgetpu edgetpu_compiler [edgetpu_compiler YOUR_MODEL.tflite]
-docker run -it --rm -v D:\Data\Study\Python\ML\Coral:/home/edgetpu edgetpu_compiler
+docker run -it --rm -v %cd%:/home/edgetpu edgetpu_compiler
