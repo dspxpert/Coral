e24c78dc3fef4a033635e79e445f9f88b03f2ea1
dspxpert <32154318+dspxpert@users.noreply.github.com>
2021-06-25 09:06:31 +0900

    Update edgetpu_docker.bat

--- edgetpu_docker.bat
+++ edgetpu_docker.bat
@@ -1,6 +1,7 @@
 @echo off
-echo To build container,
-echo docker build --tag edgetpu_compiler https://github.com/tomassams/docker-edgetpu-compiler.git
-echo To run container,
-echo docker run -it --rm -v /your/modelpath/:/home/edgetpu edgetpu_compiler [edgetpu_compiler YOUR_MODEL.tflite]
-docker run -it --rm -v %cd%:/home/edgetpu edgetpu_compiler
+REM To build container,
+REM docker build --tag edgetpu_compiler https://github.com/tomassams/docker-edgetpu-compiler.git
+REM To run container,
+REM docker run -it --rm -v /your/modelpath/:/home/edgetpu edgetpu_compiler [edgetpu_compiler YOUR_MODEL.tflite]
+if "%1" == "" docker run -it --rm -v %cd%:/home/edgetpu edgetpu_compiler
+if not "%1"=="" docker run -it --rm -v %cd%:/home/edgetpu edgetpu_compiler edgetpu_compiler %1 %2 %3 %4 %5 %6 %7 %8 %9 
