ad07846bc071ff1f6bc3e6fb248e264b069e627c
dspxpert <32154318+dspxpert@users.noreply.github.com>
2021-06-25 15:13:54 +0900

    Create edgetpu_docker.sh

---
+++ edgetpu_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# To build container,
+# docker build --tag edgetpu_compiler https://github.com/tomassams/docker-edgetpu-compiler.git
+# To run container,
+# docker run -it --rm -v $(pwd):/home/edgetpu edgetpu_compiler [edgetpu_compiler YOUR_MODEL.tflite]
+if [ $# -eq 0 ]
+then docker run -it --rm -v $(pwd):/home/edgetpu edgetpu_compiler
+else docker run -it --rm -v $(pwd):/home/edgetpu edgetpu_compiler edgetpu_compiler $1 $2 $3 $4 $5 $6 $7 $8 $9
+fi
