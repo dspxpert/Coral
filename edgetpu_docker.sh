366997fea95c4d9942dd62162fda3be811eb855e
dspxpert <32154318+dspxpert@users.noreply.github.com>
2021-06-25 20:34:05 +0900

    Update edgetpu_docker.sh

--- edgetpu_docker.sh
+++ edgetpu_docker.sh
@@ -5,5 +5,5 @@
 # docker run -it --rm -v $(pwd):/home/edgetpu edgetpu_compiler [edgetpu_compiler YOUR_MODEL.tflite]
 if [ $# -eq 0 ]
 then docker run -it --rm -v $(pwd):/home/edgetpu edgetpu_compiler
-else docker run -it --rm -v $(pwd):/home/edgetpu edgetpu_compiler edgetpu_compiler $1 $2 $3 $4 $5 $6 $7 $8 $9
+else docker run -it --rm -v $(pwd):/home/edgetpu edgetpu_compiler edgetpu_compiler $@
 fi
