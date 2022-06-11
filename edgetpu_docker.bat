cd106b09d3586457851c854200dde5e1d49d5895
dspxpert <32154318+dspxpert@users.noreply.github.com>
2022-06-11 18:22:46 +0900

    Update edgetpu_docker.bat

--- edgetpu_docker.bat
+++ edgetpu_docker.bat
@@ -16,3 +16,23 @@ if "%1" == "--install" (
 ) else (
     docker run -it --rm -v %cd%:/home/edgetpu edgetpu_compiler edgetpu_compiler %*
 )
+
+
+@echo off
+REM To build container,
+REM docker build --tag edgetpu_compiler https://github.com/tomassams/docker-edgetpu-compiler.git
+REM To run container,
+REM docker run -it --rm -v /your/modelpath/:/home/edgetpu edgetpu_compiler [edgetpu_compiler YOUR_MODEL.tflite]
+REM if "%1" == "" ( docker run -it --rm -v %cd%:/home/edgetpu edgetpu_compiler ) else ( docker run -it --rm -v %cd%:/home/edgetpu edgetpu_compiler edgetpu_compiler %* )
+
+set argc=0
+for %%x in (%*) do Set /A argc+=1
+if %argc% == 0 (
+    docker run -it --rm -v %cd%:/home/edgetpu edgetpu_compiler
+    GOTO:EOF
+)
+if "%1" == "--install" (
+    docker build --tag edgetpu_compiler https://github.com/tomassams/docker-edgetpu-compiler.git
+) else (
+    docker run -it --rm -v %cd%:/home/edgetpu edgetpu_compiler edgetpu_compiler %*
+)
